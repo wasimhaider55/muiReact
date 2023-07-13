c7bb7efdc0268fbae6673a80ce9b1f3655da6ff7
wasim Haider55 <wasimhaider14379@gmail.com>
2023-07-13 17:11:07 +0500

    created first mui with first typescript app

---
+++ react-mui-demo/src/components/MuiTypography.tsx
@@ -0,0 +1,28 @@
+import { Typography } from "@mui/material"
+
+export const MuiTypography = () => {
+    return (
+        <div>
+            <Typography variant="h1"> h1 Heading</Typography>
+            <Typography variant="h2" gutterBottom> h2 Heading</Typography>
+            <Typography variant="h3"> h3 Heading</Typography>
+            <Typography variant="h4"> h4 Heading</Typography>
+            <Typography variant="h5"> h5 Heading</Typography>
+            <Typography variant="h6"> h6 Heading</Typography>
+
+
+            <Typography variant="subtitle1">sub title 1</Typography>
+            <Typography variant="subtitle2">sub titile 2</Typography>
+
+
+            <Typography variant="body1">Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Reprehenderit, qui exercitationem dolore repudiandae ut, aliquid cupiditate ipsum aut
+                ex magnam voluptates architecto ipsam dolores culpa libero, repellat impedit iste deserunt!
+            </Typography>
+            <Typography variant="body2">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quia reprehenderit porro, inventore perferendis dolorem maiores animi. Maxime, repellendus
+                provident. Eligendi atque voluptate modi veniam quis aut nulla pariatur adipisci alias!m
+            </Typography>
+        </div>
+    )
+}
